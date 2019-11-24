d0de2b2d5080e0318555e2cc6f8db54826b4e35f
PeterArriaza <peter.arriaza@gmail.com>
2019-11-24 18:09:00 -0500

    init commit

--- src/components/status-section.test.js
+++ src/components/status-section.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import StatusSection from './status-section';
 
+
+describe('<GuessForm />', () => {
+
  it('Renders StatusSection without crashing', () => {
    shallow(<StatusSection guesses={[0]}/>);  
  });
+
+});
