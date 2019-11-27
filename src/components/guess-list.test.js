752c1f9b477f9aae9077eaa738517b8d5c98b907
PeterArriaza <peter.arriaza@gmail.com>
2019-11-26 21:07:50 -0500

    added rest of tests based on Thinkful solution (reached 2 hour mark

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -5,6 +5,9 @@ import GuessList from "./guess-list";
 
 describe("<GuessList />", () => {
   it("renders without crashing", () => {
+    // guesses prop required below because of map function in component
+    // ...but not required in other component tests to initialize component where
+    // preprocessing isn't required?
     shallow(<GuessList guesses={[1]} />);
   });
 });
